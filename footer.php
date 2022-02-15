769a91bb614d9bfe43940229e4239d9214fde00c
thibaultcoge <thibaultcoge@hotmail.fr>
2022-02-15 14:10:25 +0100

    footer v1

--- footer.php
+++ footer.php
@@ -2,6 +2,21 @@
 
 <?php wp_footer(); ?>
 
+<footer id="footer">
+    <div class="copyright">
+        <p>Need for job</p>
+    </div>
+    <div class="footer_nav">
+        <nav>
+            <ul>
+                <li><a href="#">Mentions légals</a></li>
+                <li><a href="#">Créer un CV</a></li>
+                <li><a href="#">À propos de nous</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
+
 </body>
 
 </html>
