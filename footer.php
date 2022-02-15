cb1951ea4724e4e0e30b95b0c0451b7c2b1dd385
mathisdls <mathisdllss@gmail.com>
2022-02-15 17:14:03 +0100

    footer fini

--- footer.php
+++ footer.php
@@ -3,17 +3,26 @@
 <?php wp_footer(); ?>
 
 <footer id="footer">
-    <div class="copyright">
-        <p>Need for job</p>
-    </div>
-    <div class="footer_nav">
-        <nav>
-            <ul>
-                <li><a href="#">Mentions légals</a></li>
-                <li><a href="#">Créer un CV</a></li>
-                <li><a href="#">À propos de nous</a></li>
-            </ul>
-        </nav>
+    <div class="wrap_footer">
+        <div class="copyright">
+            <a href="<?= path('/'); ?>"><img src="<?php echo get_template_directory_uri() . '/asset/img/NeedForJobv2.png' ?>" /></a>
+        </div>
+        <div class="separator_footer"></div>
+        <div class="footer_nav">
+            <nav>
+                <ul>
+                    <li><a href="#">Mentions légals</a></li>
+                    <li><a href="#">Créer un CV</a></li>
+                    <li><a href="#">À propos de nous</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="separator_footer"></div>
+        <div class="reseau_footer">
+            <a href=""><i class="fab fa-facebook-messenger"></i></a>
+            <a href=""><i class="fab fa-instagram"></i></a>
+            <a href=""><i class="fab fa-snapchat-ghost"></i></a>
+        </div>
     </div>
 </footer>
 
