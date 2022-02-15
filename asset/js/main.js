6f9d192ac6e27af09f371b02088517eba83be8ff
mathisdls <mathisdllss@gmail.com>
2022-02-15 14:42:45 +0100

    card finito

---
+++ asset/js/main.js
@@ -0,0 +1,13 @@
+const wrap = document.querySelector('.wrap_card')
+const cardImg = document.querySelectorAll('.card')
+const plus = document.querySelector('.card_plus')
+
+
+cardImg.forEach((elements) => {
+    elements.addEventListener("mouseenter", function (e) {
+        e.preventDefault();
+        console.log('oui');
+        
+    });
+  
+  });
