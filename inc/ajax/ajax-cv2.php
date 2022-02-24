bf79b63afbd49bd38545d5c5032e927ab0360e79
maxencelgy <maxencelgy@gmail.com>
2022-02-24 10:59:02 +0100

    PUSH CREATION CV + INSERTION BDD DIFFERENT TABLE

--- inc/ajax/ajax-cv2.php
+++ inc/ajax/ajax-cv2.php
@@ -59,35 +59,68 @@ function gestionFormulaireContact2()
             // 'loisirs' => $loisir,
         ),
         array('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
-        // array('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
 
     );
 
 
-     $lastCv= $wpdb->get_results(
-        $wpdb->prepare("SELECT * FROM {$wpdb->prefix}cv WHERE user_id=%s ORDER BY id DESC",$userId),
+    $lastCv = $wpdb->get_results(
+        $wpdb->prepare("SELECT * FROM {$wpdb->prefix}cv WHERE user_id=%s ORDER BY id DESC", $userId),
         ARRAY_A
     );
 
-    $cvId=$lastCv[0]['id'];
+    $cvId = $lastCv[0]['id'];
 
 
-    // foreach($formations as $formation){
+    foreach (array_combine($data['d'][0]['formation'], $data['d'][0]['dateVal']) as $formationn => $formationDate) {
+        global $wpdb;
+        $wpdb->insert(
+            $wpdb->prefix . 'formation',
+            array(
+                'cv_id' => $cvId,
+                'formation' => $formationn,
+                'date_formation' => $formationDate,
+            ),
+            array('%s', '%s', '%s')
+        );
+    }
 
-    // }
-    global $wpdb;
-    $wpdb->insert( 
-        $wpdb->prefix . 'formation',
-        array(
-            'cv_id' => $cvId,
-            'formation'=>$data['d'][0]['name'],
-        ),
-        array('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
-        // array('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
+    foreach (array_combine($data['d'][0]['experienceVal'], $data['d'][0]['dateExp']) as $formationn => $formationDate) {
+        global $wpdb;
+        $wpdb->insert(
+            $wpdb->prefix . 'experience',
+            array(
+                'cv_id' => $cvId,
+                'experience' => $formationn,
+                'date_experience' => $formationDate,
+            ),
+            array('%s', '%s', '%s')
+        );
+    }
 
-    );
+    foreach (array_combine($data['d'][0]['langue'], $data['d'][0]['niveau']) as $formationn => $formationDate) {
+        global $wpdb;
+        $wpdb->insert(
+            $wpdb->prefix . 'langue',
+            array(
+                'cv_id' => $cvId,
+                'langue' => $formationn,
+                'niveau_langue' => $formationDate,
+            ),
+            array('%s', '%s', '%s')
+        );
+    }
 
-
+    foreach ($data['d'][0]['loisir'] as $loisirs) {
+        global $wpdb;
+        $wpdb->insert(
+            $wpdb->prefix . 'passion',
+            array(
+                'cv_id' => $cvId,
+                'passion' => $loisirs,
+            ),
+            array('%s', '%s')
+        );
+    }
     $success = true;
 
     $dataa = array(
